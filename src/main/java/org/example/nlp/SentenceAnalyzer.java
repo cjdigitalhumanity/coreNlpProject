9c285c7e399e55c10df7435311f3cbc3b14a89e7
cjdigitalhumanity <dighumanitymd@gmail.com>
2023-10-12 06:52:30 -0400

    Sentence Analyzer Commit

--- src/main/java/org/example/nlp/SentenceAnalyzer.java
+++ src/main/java/org/example/nlp/SentenceAnalyzer.java
@@ -9,34 +9,24 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 
-public class SentenceAnalyzer {
-    public static void main(String[] args){
-        try {
-            File myObj = new File("C:\\Users\\cjdan\\OneDrive\\Documents\\testFile.txt");
-            Scanner myReader = new Scanner(myObj);
+    public class SentenceAnalyzer {
+        public static void main(String[] args){
 
-            String data = myReader.nextLine();
 
-            myReader.close();
+                StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
+                String text = "This is a test for the sentences. Find Them all";
 
-        StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
-       // String text = "This is a test for the sentences. Find Them all";
+                CoreDocument coreDocument = new CoreDocument(text);
 
-        CoreDocument coreDocument = new CoreDocument(data);
+                stanfordCoreNLP.annotate(coreDocument);
 
-        stanfordCoreNLP.annotate(coreDocument);
+                List<CoreSentence> sentences = coreDocument.sentences();
 
-        List<CoreSentence> sentences = coreDocument.sentences();
+                int i = 0;
 
-        int i = 0;
-
-          for(CoreSentence sentence : sentences){
-              i+=1;
-              System.out.println("Sentence" + i + " " + ": " + sentence.toString());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+                for(CoreSentence sentence : sentences) {
+                    i += 1;
+                    System.out.println("Sentence" + i + " " + ": " + sentence.toString());
+                }
         }
     }
-}
