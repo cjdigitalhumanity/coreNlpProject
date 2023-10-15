f7f6ee77529c67c7b34bb3da47b76309fe3a4407
cjdigitalhumanity <dighumanitymd@gmail.com>
2023-10-15 09:19:53 -0400

    Part of Speech tool Commit

---
+++ src/main/java/org/example/nlp/PartOfSpeechAnalyzer.java
@@ -0,0 +1,33 @@
+package org.example.nlp;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class PartOfSpeechAnalyzer {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("C:\\Users\\cjdan\\OneDrive\\Documents\\testFile.txt");
+            Scanner myReader = new Scanner(myObj);
+
+            String data = myReader.nextLine();
+
+            myReader.close();
+
+            CoreDocument coreDocument = new CoreDocument(data);
+
+
+
+        } catch (
+                FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
